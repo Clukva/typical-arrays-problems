eceebff058ce1d678d1790852d192b5f30965704
Clukva <yura4-1-1@mail.ru>
2021-03-02 13:32:19 +0300

    feat: add solution for task Typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
+   if(Array.isArray(array) && array.length){
+   return Math.min(...array)
+  }
   return 0;
+  
 }
+console.log(exports.min)
 
 exports.max = function max (array) {
+  if(Array.isArray(array) && array.length){
+    return Math.max(...array)
+   }
   return 0;
+   
 }
 
 exports.avg = function avg (array) {
+  if(Array.isArray(array) && array.length){
+  let sum = array.reduce((a,b) => a+b);
+  let result = sum / array.length;
+  return result ;
+  }
   return 0;
 }
